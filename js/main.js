a1409cfd9d685df83ffd0fd3facd4987a0965b76
Ionela Pastrama <ionipas@yahoo.com>
2018-09-20 20:38:12 +0300

    modified the function that create HTML for restaurant images

--- js/main.js
+++ js/main.js
@@ -160,7 +160,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
-  image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.src = DBHelper.imageSmallUrlForRestaurant(restaurant);
   li.append(image);
 
   const name = document.createElement('h1');
